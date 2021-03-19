786b1eaab466dcaca283434f8d3c7c027a8bdda6
Marc Pomar <marc@corecode.school>
2021-03-19 19:33:16 +0100

    fix: kirbic doamin

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,13 @@ import { UserProvider as Auth0UserProvider } from "@auth0/nextjs-auth0";
 import { useUser as auth0useUser } from "@auth0/nextjs-auth0";
 
 // Automatically set kirbic domain
-process.env.AUTH0_ISSUER_BASE_URL = "https://YOUR_AUTH0_DOMAIN.auth0.com";
+process.env.AUTH0_ISSUER_BASE_URL = "https://kirbic.eu.auth0.com";
 
 // A long secret value used to encrypt the session cookie
 process.env.AUTH0_SECRET = process.env.KIRBIC_SECRET;
 
 // The base url of your application
 process.env.AUTH0_BASE_URL = process.env.KIRBIC_BASE_URL;
-
-// The url of your Auth0 tenant domain
-process.env.AUTH0_ISSUER_BASE_URL = "https://kirbic.auth0.com";
 
 // Your Auth0 application's Client ID
 process.env.AUTH0_CLIENT_ID = process.env.KIRBIC_CLIENT_ID;
